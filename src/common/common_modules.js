f069e0d7b3ee224eabd40eb75fd6e5d4b867d6e2
Tim-Elias <timofeev.i.v@mail.ru>
2021-02-24 12:57:33 +0700

    no

--- src/common/common_modules.js
+++ src/common/common_modules.js
@@ -2,10 +2,10 @@ import axios from 'axios/index'
 
 const get_data = (url, data) => {
   return new Promise((resolve, reject) => {
-    axios.post(`http://kinetika-1c.tmweb.ru/http/hs/agent/${url}/post`,
+    axios.post(`https://kinetika-1c.tmweb.ru/http/hs/agent/${url}/post`,
       JSON.stringify(data),
       {
-        headers: { 'Content-Type': 'text/plain' }
+        headers: { 'Content-Type': 'text/plain' } 
       }
 
     ).then((result) => {
