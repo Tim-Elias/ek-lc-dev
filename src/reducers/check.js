7738a2e9091eb70e825003af7a59948c3e6df9a0
Tim-Elais <timofeev.i.v@mail.ru>
2021-03-04 16:18:56 +0700

    no

--- src/reducers/check.js
+++ src/reducers/check.js
@@ -11,6 +11,7 @@ const initialState = {
 export default function dispatch(state = initialState, action) {
     switch (action.type) {
         case 'set_check_list': return { ...state, list: action.payload }
+        case 'reset_check_list': return initialState
         case 'set_check_data': return {
             ...state, check_data: {
                 ...action.payload, 
